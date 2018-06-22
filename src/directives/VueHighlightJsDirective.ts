c13dcfdc25d50826f45118c4b3509e23ed08b6b7
Rajasekar <e.rajasekar@gmail.com>
2018-06-22 07:19:11 -0400

    Handled single backtick code

--- src/directives/VueHighlightJsDirective.ts
+++ src/directives/VueHighlightJsDirective.ts
@@ -1,35 +1,35 @@
 import hljs from 'highlight.js';
 
 export const VueHighlightJsDirective = {
-    bind(el: any, binding: any) {
+    bind(el: Element, binding: any) {
 
         // if a value is directly assigned to the directive, use this
         // instead of the element content.
-        if (binding.value) {
-            el.innerHTML = binding.value;
-        } else {
-            el.innerHTML = el.textContent;
-        }
+        el.innerHTML = binding.value ? binding.value : el.textContent;
         // on first bind, highlight all targets
         const targets = el.querySelectorAll('code');
-        targets.forEach((target: any) => {
-            hljs.highlightBlock(target);
+        targets.forEach((target: Element) => {
+            // Check for class attribute to not apply hljs for single backtick code.
+            if (target.hasAttribute('class')) {
+                hljs.highlightBlock(target);
+            }
         });
 
     },
-    componentUpdated(el: any, binding: any) {
+    componentUpdated(el: Element, binding: any) {
         // after an update, re-fill the content and then highlight
 
         // if a value is directly assigned to the directive, use this
         // instead of the element content.
-        if (binding.value) {
-            el.innerHTML = binding.value;
-        } else {
-            el.innerHTML = el.textContent;
-        }
+        el.innerHTML = binding.value ? binding.value : el.textContent;
+
         const targets = el.querySelectorAll('code');
-        targets.forEach((target: any) => {
-            hljs.highlightBlock(target);
+        targets.forEach((target: Element) => {
+            // Check for class attribute to not apply hljs for single backtick code.
+            if (target.hasAttribute('class')) {
+                hljs.highlightBlock(target);
+            }
+
         });
     },
 };
