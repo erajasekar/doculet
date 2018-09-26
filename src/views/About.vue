64b65ede346b1083252374d0611af19b8874f242
Rajasekar <e.rajasekar@gmail.com>
2018-09-26 10:24:03 -0400

    Initial version of About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+
+        <vue-headful
+                :title="title"
+                :description="description"
+                :keywords="keywords"
+        />
+
+        <b-jumbotron id="about-block1" header="About Doculet" fluid
+                     lead="I am working on Doculet as side project and my goal to make it easier share and embed code examples for any website.">
+
+            <h2>
+                Why am I building this project?
+            </h2>
+
+            <h2>
+                Github Integration
+            </h2>
+
+            <h2>
+                FAQ
+            </h2>
+            <h4>
+                Is Doculet free to Use?
+            </h4>
+            <h4>
+                Where are my documents saved?
+            </h4>
+            <h4>
+                Why do I need to grant write Gist permission?
+            </h4>
+            <h4>
+                I don't have Github account. What should I do?
+            </h4>
+            <h4>
+                Why not use standard markdown instead of AsciiDoc ?
+            </h4>
+            <h4>
+                When will it be ready for use?
+            </h4>
+        </b-jumbotron>
+
+        <bottom-footer></bottom-footer>
+    </div>
 </template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import Constants from '../utils/constants';
+    import BottomFooter from '@/components/BottomFooter.vue';
+
+    @Component({
+        components: {
+            BottomFooter,
+        },
+    })
+    export default class About extends Vue {
+
+        private title = "About Doculet";
+        private description = Constants.DOCULET_DESCRIPTION;
+        private keywords = Constants.DOCULET_SEO_KEYWORDS;
+    }
+</script>
+
+<style scoped lang="scss">
+    @import '../assets/scss/about.scss';
+</style>
