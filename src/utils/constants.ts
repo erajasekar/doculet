1aeef842f62d19c7ce52cb9f2f54d605caba17cb
Rajasekar <e.rajasekar@gmail.com>
2018-09-27 06:28:37 -0400

    Updated getting started gist

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,29 +1,10 @@
 export default class Constants {
     public static readonly ACCESS_TOKEN_PROPERTY = 'accessToken';
     public static readonly DB_COLLECTION_DOCULENTS = 'doculets';
-    public static readonly ON_LOAD_DOC_NAME = 'Getting Started.adoc';
-    public static readonly ON_LOAD_DOC_CONTENT = 'Welcome to AsciiDocLIVE!\n' +
-        '------------------------\n' +
-        '\n' +
-        'AsciiDocLIVE is a *free online http://www.methods.co.nz/asciidoc/[AsciiDoc^]\n' +
-        'editor*.\n' +
-        '\n' +
-        '* Just type AsciiDoc source text into the *left* pane,\n' +
-        '* ...and the live preview appears in the *right* pane!\n' +
-        '\n' +
-        'What\'s AsciiDoc?\n' +
-        '~~~~~~~~~~~~~~~~~\n' +
-        '\n' +
-        '[source,ruby]\n' +
-        '.app.rb\n' +
-        '----\n' +
-        'require \'sinatra\'\n' +
-        'get \'/hi\' do\n' +
-        ' "Hello World!"\n' +
-        'end\n' +
-        '----';
+    public static readonly ON_LOAD_DOC_NAME = 'User Guide.adoc';
+    public static readonly ON_LOAD_DOC_CONTENT = 'Loading';
     public static readonly NEW_DOC_GIST_ID = 'e322756b428595516bc471c81c2081bf';
-    public static readonly GETTING_STARTED_DOC_GIST_ID = '15d05096e7076183d74469ed32ba234f';
+    public static readonly GETTING_STARTED_DOC_GIST_ID = '1f3465ad9f26a24c52e9b61f64aa7670';
     public static readonly VIEW_URL_PATTERN = '/doc/';
     public static readonly MENU_ICON_COLOR = '#1E2C40';
     public static readonly GITHUB_API_URL = 'https://api.github.com/gists/';
