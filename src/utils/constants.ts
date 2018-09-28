7ef5638f0de82d268b0dd24edad35899f60ae054
Rajasekar <e.rajasekar@gmail.com>
2018-09-27 23:08:20 -0400

    Updated new document gist

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export default class Constants {
     public static readonly DB_COLLECTION_DOCULENTS = 'doculets';
     public static readonly ON_LOAD_DOC_NAME = 'User Guide.adoc';
     public static readonly ON_LOAD_DOC_CONTENT = 'Loading';
-    public static readonly NEW_DOC_GIST_ID = 'e322756b428595516bc471c81c2081bf';
+    public static readonly NEW_DOC_GIST_ID = '8d03c78d367928f569d9e3dbad8644fa';
     public static readonly GETTING_STARTED_DOC_GIST_ID = '1f3465ad9f26a24c52e9b61f64aa7670';
     public static readonly VIEW_URL_PATTERN = '/doc/';
     public static readonly MENU_ICON_COLOR = '#1E2C40';
@@ -11,7 +11,7 @@ export default class Constants {
     public static readonly GITHUB_DOCULET_OWNER_ID = '629276';
     public static readonly DOCULET_TITLE = 'Doculet - Easily share and embed code examples in your website';
     public static readonly DOCULET_DESCRIPTION = 'Doculet is a free open source project to easily create, ' +
-        'share and embed code examples in your blog, ' +
+        'share and embed code examples on your blog, ' +
         'medium articles or any of your website.';
     public static readonly DOCULET_SEO_KEYWORDS = 'code, share, embed, blog, website, ' +
         'comments, programming, computer science';
