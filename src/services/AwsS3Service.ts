0a1db86d8bb96a14be812f258817dba41765da98
Rajasekar <e.rajasekar@gmail.com>
2018-10-02 07:16:05 -0400

    Got list bucket to work

--- src/services/AwsS3Service.ts
+++ src/services/AwsS3Service.ts
@@ -1,11 +1,12 @@
 import {config} from '../config/config';
-import AWS from 'aws-sdk'
+import AWS, {S3} from 'aws-sdk'
 import strStream from 'string-to-stream';
 import {ManagedUpload} from "aws-sdk/lib/s3/managed_upload";
 
 AWS.config.update({
     accessKeyId: config.aws.accessKey,
     secretAccessKey: config.aws.secretAccessKey,
+    region: config.aws.s3BucketRegion,
 });
 
 export class AwsS3Service {
@@ -14,16 +15,26 @@ export class AwsS3Service {
 
     public publishDoc(docId: string, html: string) {
 
-        const params = {
+       /* const params = {
             Bucket: config.aws.s3BucketName || 'Bucket name not found in config',
             Body : strStream(html),
             Key : `doc/${docId}/index.html`,
+        };*/
+
+        const params = {
+            Bucket: config.aws.s3BucketName || 'Bucket name not found in config',
+            Delimiter: '/',
         };
 
         console.log(config.aws);
-        console.log(html);
+      //  console.log(html);
 
-        this.s3.upload(params, function (err: Error, data: ManagedUpload.SendData) {
+        this.s3.listObjects(params, function(err: Error, data: S3.Types.ListObjectsOutput) {
+            console.log(data);
+
+        });
+
+        /*this.s3.upload(params, function (err: Error, data: ManagedUpload.SendData) {
             //handle error
             if (err) {
                 console.log("Error", err);
@@ -33,7 +44,7 @@ export class AwsS3Service {
             if (data) {
                 console.log("Uploaded in:", data.Location);
             }
-        });
+        });*/
     }
 }
 
