523358044a45bc2413e732c7ff9f95b5a21311e3
Rajasekar <e.rajasekar@gmail.com>
2018-10-18 07:01:17 -0400

    Better customization

--- src/components/DocShare.vue
+++ src/components/DocShare.vue
@@ -93,7 +93,7 @@
             logDebug(`DocId to share ${docId}, oldValue: ${oldValue} , docId in store ${this.docId}`);
             this.updateDocId(docId);
             this.updateDocEdited(false);
-            this.updatePublishLocation('http://localhost:8080/embed/stream.html');
+            this.updatePublishLocation('http://localhost:8080/embed/twocode.html');
             /* if (!this.publishLocation) {
                 dbService.getPublishLocation(docId).then( (location: string) => {
                     this.updatePublishLocation(location);
