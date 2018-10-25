d7ccb8d94fbb52cf516578b2d664a59e1a082b80
Rajasekar <e.rajasekar@gmail.com>
2018-10-25 07:13:28 -0400

    Got basic theme switching working

--- public/js/embed/v0.4.0/embed.js
+++ public/js/embed/v0.4.0/embed.js
@@ -1,22 +1,59 @@
-var clip = new ClipboardJS('.copy-btn', {
-    target: function(trigger) {
-      return trigger.parentNode.nextElementSibling;
+(function() {
+    var clip = new ClipboardJS('.copy-btn', {
+        target: function(trigger) {
+        return trigger.parentNode.nextElementSibling;
+        }
+    });
+
+    clip.on('success', function(event) {
+        event.clearSelection();
+        event.trigger.attributes['tooltip'].value = "Copied";
+        window.setTimeout(function() {
+            event.trigger.attributes['tooltip'].value = "Copy";
+        }, 5000);
+    });
+
+    var toggleThemeBtn = document.getElementById("toggle-theme-btn");
+    toggleThemeBtn.addEventListener("click", toggleTheme);
+    var primaryTheme = toggleThemeBtn.dataset.theme;
+    var secondaryTheme = primaryTheme == "light" ? "dark" : "light";
+
+    var codeStyleElement = document.getElementById("code-theme");
+    var codeStylePrimary = codeStyleElement.getAttribute("href");
+    var codeStyleSecondary = getSecondaryHref(codeStylePrimary, true);
+    console.log(codeStylePrimary, codeStyleSecondary);
+
+    var asciidocStyleElement = document.getElementById("asciidoc-theme");
+    var asciidocStylePrimary = asciidocStyleElement.getAttribute("href");
+    var asciidocStyleSecondary = getSecondaryHref(asciidocStylePrimary, false);
+    console.log(asciidocStylePrimary, asciidocStyleSecondary);
+  
+    function toggleTheme(){
+        // TODO update tooltip text
+        var icon = toggleThemeBtn.querySelector("i");
+        var newTheme = toggleThemeBtn.dataset.theme == primaryTheme? secondaryTheme : primaryTheme ;
+        toggleThemeBtn.dataset.theme = newTheme;
+        icon.classList.toggle("fa-moon", newTheme == "light");
+        icon.classList.toggle("fa-sun", newTheme == "dark");
+        var isPrimary = newTheme == primaryTheme;
+        codeStyleElement.href = isPrimary ? codeStylePrimary : codeStyleSecondary;
+        asciidocStyleElement.href = isPrimary ? asciidocStylePrimary : asciidocStyleSecondary ;
     }
-});
 
-clip.on('success', function(event) {
-    event.clearSelection();
-    event.trigger.attributes['tooltip'].value = "Copied";
-    window.setTimeout(function() {
-        event.trigger.attributes['tooltip'].value = "Copy";
-    }, 5000);
-});
+    function getSecondaryHref(primaryHref, useDefault){
+        var lastSlashIndex = primaryHref.lastIndexOf("/") + 1;
+        var pathLastSegment = primaryHref.substr(lastSlashIndex);
+        var basePath =  primaryHref.substr(0, lastSlashIndex);
+        var replaced;
 
-function toggleTheme(target){
-    var icon = target.querySelector("i");
-    icon.classList.remove("fa-moon");
-    icon.classList.add("fa-sun");
-    console.log(icon.classList);
-    document.getElementById('code-theme').href = '/css/embed/v0.4.0/highlightjs/default.dark.min.css';
-    document.getElementById('asciidoc-theme').href = '/css/embed/v0.4.0/asciidoc/colony.dark.css';
-}
+        if (useDefault) {
+            var dotFirstIndex = pathLastSegment.indexOf(".");
+            var themeName = pathLastSegment.substr(0, dotFirstIndex);
+            replaced =  pathLastSegment.replace(themeName, "default").replace(primaryTheme, secondaryTheme)
+        } else {
+            replaced =  pathLastSegment.replace(primaryTheme, secondaryTheme);
+        }
+        var secondaryHref = basePath + replaced;
+        return secondaryHref;
+    }
+})();
